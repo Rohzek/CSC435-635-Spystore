7637a53a9c25de247c70c35a610a6d2ff5b01eb6
Rohzek <rohzek@gmail.com>
2018-03-14 16:17:47 -0500

    Filling in Classes
    Added some necessary classes to Core, Domain, Repository, and Services

---
+++ SpyStore/Repository/ICategoryRepository.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Repository
+{
+    public interface ICategoryRepository : IRepository<Cateogry>
+    {
+        IEnumerable<Category> GetAllWithProducts();
+        Category GetOneWithProducts(int? id);
+    }
+}
---
+++ SpyStore/Services/CategoryService.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace Services
+{
+    public class CategoryService
+    {
+        private IRepository<Category> _Repository;
+
+        public CategoryService()
+        {
+            _Repository = new CategoryRepository(new DbContetOptions<StoreContext>());
+        }
+    }
+}
